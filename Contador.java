8aa2be7990e94aca0e7319443a486787bb3de3a9
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2024-09-24 23:32:06 -0300

    add files

---
+++ Contador.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = myObj.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = myObj.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+    static void contar(int p1, int p2 ) throws ParametrosInvalidosException {
+        if(p1 > p2){
+            throw new ParametrosInvalidosException();
+        }
+        for(int i = p1; i <= p2; i++){
+            System.out.println("Imprimindo o número: " + i);
+        }
+    }
+}
